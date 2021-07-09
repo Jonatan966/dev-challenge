e8476b2ae74cb9f2a9dc3a0456ec15a7f40a5c1e
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-09 14:33:46 -0300

    feat(server): authentication routes

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
+
 import { createUserController } from './use-cases/create-user'
+import { authenticateUserController } from './use-cases/authenticate-user'
+import { refreshTokenUserController } from './use-cases/refresh-token-user'
 
 const routes = Router()
 
 routes.post('/users', createUserController.handle)
+routes.post('/login', authenticateUserController.handle)
+routes.post('/refresh-token', refreshTokenUserController.handle)
 
 export { routes }
