eb42f2d9e14ca222037547fbdf58e11847d7d166
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-11 13:25:04 -0300

    feat(web):dynamic informations in user info header

--- packages/web/src/components/user-info-header.tsx
+++ packages/web/src/components/user-info-header.tsx
@@ -1,14 +1,19 @@
 import { Text, Box, Image } from '@chakra-ui/react'
+
 import { PrimaryHeader } from './primary-header'
 import { ProgressInfoCard } from './progress-info-card'
 
+import { useAuth } from '../contexts/auth-context'
+
 export function UserInfoHeader(): JSX.Element {
+  const { user } = useAuth()
+
   return (
     // top={0} zIndex={99} background="white"
     <Box as="header" position="sticky">
       <PrimaryHeader paddingBottom="10">
         <Text fontSize="2xl" color="white">
-          Olá, <strong>Jonatan</strong>
+          Olá, <strong>{user?.name}</strong>
         </Text>
         <Image
           boxSize="50"
@@ -20,13 +25,15 @@ export function UserInfoHeader(): JSX.Element {
       </PrimaryHeader>
 
       <ProgressInfoCard
-        value={50}
-        title="Nível 2"
+        value={user.currentExperience}
+        max={user.currentExperience + user.experienceToNextLevel}
+        title={`Nível ${user.currentLevel}`}
         marginLeft="5"
         marginRight="5"
         translateY="-20%"
       >
-        Faltam <strong>250xp</strong> para o <strong>Nível 3</strong>
+        Faltam <strong>{user.experienceToNextLevel}xp</strong> para o{' '}
+        <strong>Nível {user.currentLevel + 1}</strong>
       </ProgressInfoCard>
     </Box>
   )
