acafc571c33be47d89e40e294199cd2fa7c160aa
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-15 17:59:51 -0300

    feat(web): back button in challenge header

--- packages/web/src/components/challenge-header.tsx
+++ packages/web/src/components/challenge-header.tsx
@@ -1,4 +1,7 @@
-import { Image, Text } from '@chakra-ui/react'
+import { Button, Image, Text } from '@chakra-ui/react'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { useHistory } from 'react-router-dom'
+
 import { PrimaryHeader } from './primary-header'
 
 interface ChallengeHeaderProps {
@@ -10,8 +13,13 @@ export function ChallengeHeader({
   title,
   iconUrl
 }: ChallengeHeaderProps): JSX.Element {
+  const history = useHistory()
+
   return (
     <PrimaryHeader alignItems="center" as="header">
+      <Button onClick={() => history.goBack()}>
+        <ChevronLeftIcon w={8} h={8} />
+      </Button>
       <Text flex={1} textAlign="center" fontSize="xl" color="white">
         {title}
       </Text>
