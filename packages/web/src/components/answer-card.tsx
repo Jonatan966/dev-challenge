dff080ac4e14c087ce844060a4c0f929f6e41a43
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-15 17:59:10 -0300

    feat(web): functional answer card

--- packages/web/src/components/answer-card.tsx
+++ packages/web/src/components/answer-card.tsx
@@ -1,13 +1,45 @@
-import { Checkbox, Text } from '@chakra-ui/react'
+import { Checkbox } from '@chakra-ui/react'
+
+import { Answer } from '@dev-challenge/entities'
 import { PrimaryBox } from './primary-box'
 
-export function AnswerCard(): JSX.Element {
+interface AnswerCardProps {
+  answer: Answer
+  isDisabled: boolean
+  onCheck: (answerId: string) => void
+}
+
+export function AnswerCard({
+  answer: { id, title, isChecked, isCorrect },
+  isDisabled = false,
+  onCheck
+}: AnswerCardProps): JSX.Element {
+  const isConfirmed = typeof isCorrect === 'boolean'
+  const statusColor = isConfirmed ? (isCorrect ? 'green' : 'red') : 'yellow'
+
+  const primaryBoxStatusStyle = {
+    background: `${statusColor}.100`,
+    borderColor: `${statusColor}.500`
+  }
+
   return (
-    <PrimaryBox display="flex" gridGap={4}>
-      <Text flex="1" fontSize="sm">
-        Kit de desenvolvimento de interface de usuário
-      </Text>
-      <Checkbox size="lg" colorScheme="green" />
+    <PrimaryBox
+      display="flex"
+      gridGap={4}
+      cursor="pointer"
+      onClick={() => !isDisabled && onCheck(id)}
+      {...(!isConfirmed ? {} : primaryBoxStatusStyle)}
+    >
+      <Checkbox
+        size="lg"
+        colorScheme={statusColor}
+        flex={1}
+        fontSize="sm"
+        isChecked={!!isChecked}
+        disabled={isDisabled}
+      >
+        {title}
+      </Checkbox>
     </PrimaryBox>
   )
 }
