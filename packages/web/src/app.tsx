a0b3073b258ed7578a69dbb4fd0e0228c03ab816
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-15 18:00:24 -0300

    feat(web): implement loading provider

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { AppLogoHeader } from './components/app-logo-header'
 import { AuthVerificationRoute } from './components/private-route'
 import { AuthProvider } from './contexts/auth-context'
+import { ChallengeProvider } from './contexts/challenge-context'
+import { LoadingProvider } from './contexts/loading-context'
 import { ChallengeDetailsPage } from './pages/challenge-details'
 import { ChallengeQuestionPage } from './pages/challenge-question'
 import { HomePage } from './pages/home'
@@ -18,42 +20,46 @@ export function App(): JSX.Element {
   return (
     <BrowserRouter>
       <ChakraProvider>
-        <AuthProvider>
-          <Switch>
-            <Route path="/auth">
-              <AppLogoHeader />
-              <Switch>
+        <LoadingProvider>
+          <AuthProvider>
+            <Switch>
+              <Route path="/auth">
+                <AppLogoHeader />
+                <Switch>
+                  <AuthVerificationRoute
+                    exact
+                    path="/auth"
+                    component={LoginPage}
+                    redirectAfterVerified="/challenges"
+                  />
+                  <Route exact path="/auth/register" component={RegisterPage} />
+                  <Redirect from="/auth/*" to="/404" />
+                </Switch>
+              </Route>
+
+              <AuthVerificationRoute
+                exact
+                path="/challenges"
+                component={HomePage}
+                isPrivate
+              />
+              <ChallengeProvider>
                 <AuthVerificationRoute
                   exact
-                  path="/auth"
-                  component={LoginPage}
-                  redirectAfterVerified="/challenges"
+                  path="/challenges/:id"
+                  component={ChallengeDetailsPage}
+                  isPrivate
                 />
-                <Route exact path="/auth/register" component={RegisterPage} />
-                <Redirect from="/auth/*" to="/404" />
-              </Switch>
-            </Route>
-
-            <AuthVerificationRoute
-              exact
-              path="/challenges"
-              component={HomePage}
-              isPrivate
-            />
-            <AuthVerificationRoute
-              exact
-              path="/challenges/:id"
-              component={ChallengeDetailsPage}
-              isPrivate
-            />
-            <AuthVerificationRoute
-              exact
-              path="/challenge"
-              component={ChallengeQuestionPage}
-              isPrivate
-            />
-          </Switch>
-        </AuthProvider>
+                <AuthVerificationRoute
+                  exact
+                  path="/challenge"
+                  component={ChallengeQuestionPage}
+                  isPrivate
+                />
+              </ChallengeProvider>
+            </Switch>
+          </AuthProvider>
+        </LoadingProvider>
       </ChakraProvider>
     </BrowserRouter>
   )
